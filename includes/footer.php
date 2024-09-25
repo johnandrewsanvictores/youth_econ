a83a13b7f575a07a4c0a64c909bdfc81efe947f2
johnandrewsanvictores <sanvictoresjohnandrewe@gmail.com>
2024-09-25 21:39:57 +0800

    added footer and nav

---
+++ includes/footer.php
@@ -0,0 +1,56 @@
+<footer>
+    <div class="upper">
+        <div class="contact-section">
+            <h5>CONTACT US</h5>
+            <p>(+63)*** **** ***</p>
+            <p>Brgy Gulang-Gulang Lucena City</p>
+        </div>
+        <div class="social-section">
+            <h5>SOCIAL</h5>
+            <a href="https://facebook.com/profile.php?id=100085055773212" target="_blank">
+                <i class="fab fa-facebook"></i>
+                <p>skgulanggulang</p>
+            </a>
+            <a href="https://facebook.com/profile.php?id=100085055773212" target="_blank">
+                <i class="fab fa-instagram"></i>
+                <p>skgulanggulang@gmail.com</p>
+            </a>
+        </div>
+
+        <a href="#">See developers</a>
+    </div>
+
+    <div class="copyright-container">
+        <p>© 2024, <span class="primary-text">Youth Econ</span>. All Rights Reserved.</p>
+    </div>
+</footer>
+
+<style>
+    footer {
+        display: flex;
+        flex-direction: column;
+        gap: 2em;
+        align-items: center;
+        padding: 3em;
+        background: var(--primary);
+        color: var(--font-white);
+    }
+
+    .upper {
+        display: flex;
+        justify-content: space-between;
+        gap: 3em;
+        flex-wrap: wrap;
+    }
+
+    .upper>div {
+        display: flex;
+        flex-direction: column;
+        gap: 0.5em;
+    }
+
+    .social-section a {
+        display: flex;
+        gap: 1em;
+    }
+</style>
