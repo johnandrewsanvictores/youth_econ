60ae42aefe0cb0e8237a622111e5f6b40f75db6b
johnandrewsanvictores <sanvictoresjohnandrewe@gmail.com>
2024-09-25 22:14:38 +0800

    'test'

---
+++ admin/pages/login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+
+<body>
+    <section class="main-section">
+        <div class="main-container">
+            <div class="logo-section">
+                <img src="../images/logo.jpg" alt="Logo">
+            </div>
+            <div class="form-section">
+                <form action="" method="POST">
+                    <h1>Admin Login</h1>
+                    <input type="text" name="username" id="username">
+                    <input type="password" name="password" id="password">
+                    <button type="submit"></button>
+                </form>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
