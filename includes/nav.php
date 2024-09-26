f95de382a8fd2ff7adf1600e6b56765fe77282d1
Congroooooo <nckoblms@gmail.com>
2024-09-26 14:53:31 +0800

    added gallery link

--- includes/nav.php
+++ includes/nav.php
@@ -9,6 +9,7 @@
     <li><a href="about.php">About</a></li>
     <li><a href="workers.php">Worker</a></li>
     <li><a href="business.php">Business</a></li>
+    <li><a href="gallery.php">Gallery</a></li>
   </ul>
 </nav>
 
