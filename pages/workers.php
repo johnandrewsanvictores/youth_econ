e1e2528675073881e8d2fdf085692b3d4e1690fc
unknown <ms.a.smllcsn@outlook.com>
2024-10-01 00:01:34 +0800

    heading langs po, salamt andrew

--- pages/workers.php
+++ pages/workers.php
@@ -3,9 +3,70 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+    <link rel="stylesheet" href="../global.css">
+    <link rel="stylesheet" href="../public/css/workers.css">
+
+
     <title>Workers</title>
+
 </head>
 <body>
-    <h1>fd</h1>
+    
+    <?php
+    include('../includes/nav.php');
+    ?>
+
+    <section class="main_container">
+
+    <div class="head">
+
+    <div class="title">
+
+    <h2>Workers</h2>
+    <p id="p_title">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+
+    </div>
+
+    
+    <div class="function">
+
+    <form action="">
+
+    <input type="search" id="searchbar" placeholder="Search..">
+    <button><i class="fas fa-search"></i></button>
+    <button>Filter</button>
+
+    </form>
+
+
+    </div>
+
+    </div>
+
+    <div class="body">
+
+    <?php
+    include('../includes/worker_card.php');
+    include('../includes/worker_card.php');
+    include('../includes/worker_card.php');
+    include('../includes/worker_card.php');
+    include('../includes/worker_card.php');
+    include('../includes/worker_card.php');
+    ?>
+
+
+
+    </div>
+
+
+    </section>
+
+    <?php
+    include('../includes/footer.php');
+    ?>
+
+
+
 </body>
 </html>
