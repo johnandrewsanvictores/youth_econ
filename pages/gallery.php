059a44d2ec65ef553a10ab174a71206c8243b7ff
Congroooooo <nckoblms@gmail.com>
2024-10-22 15:34:26 +0800

    Made changes

--- pages/gallery.php
+++ pages/gallery.php
@@ -1,11 +1,125 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gallery</title>
-</head>
-<body>
-    
-</body>
-</html>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+        <link rel="stylesheet" href="../global.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightgallery-js/1.4.0/css/lightgallery.min.css" />
+        
+        <link rel="stylesheet" href="../public/css/Gallery.css">
+        <title>Gallery</title>
+    </head>
+    <body>
+
+    <?php
+        include('../includes/nav.php');
+        ?>
+
+    <section class ="big_container">
+        <h2>Product Gallery</h2> 
+            <div class = "flex_container">
+                <button class="logo-button">
+                    <img src="../public/images/logo.jpg" alt="Logo"> Clothing
+                </button>
+
+                <button class="logo-button">
+                    <img src="../public/images/logo.jpg" alt="Logo"> Arts & Crafts
+                </button>
+
+                <button class="logo-button">
+                    <img src="../public/images/logo.jpg" alt="Logo"> Food
+                </button>
+
+                <button class="logo-button">
+                    <img src="../public/images/logo.jpg" alt="Logo"> Digital
+                </button>
+
+                <button class="logo-button">
+                    <img src="../public/images/logo.jpg" alt="Logo"> AAAA
+                </button>
+
+                <button class="logo-button">
+                    <img src="../public/images/logo.jpg" alt="Logo"> BBBB
+                </button>
+
+                <button class="logo-button">
+                    <img src="../public/images/logo.jpg" alt="Logo"> CCCC
+                </button>
+
+                <button class="logo-button">
+                    <img src="../public/images/logo.jpg" alt="Logo"> DDDD
+                </button>
+
+                <button class="next-button">Next</button>
+            </div>
+
+            <div class="container">
+                <div class="photo-gallery">
+
+                    <div class="column">
+                        <div class="photo">
+                            <img src="https://essentialhoodies.ca/wp-content/uploads/2024/03/New-Project-18.jpg" alt="">
+                        </div>
+
+                        <div class="photo">
+                            <img src="https://smmarkets.ph/media/catalog/product/2/0/20436418_jack_jill-piattos_sprsized_chseprty-170g_copy_.png" alt="">
+                        </div>
+
+                        <div class="photo">
+                            <img src="https://market99.com/cdn/shop/files/decorative-ceramic-flower-vase-green-bottle-glossy-vases-1_2048x2048.jpg?v=1697015177" alt="">
+                        </div>
+                    </div>
+
+                    <div class="column">
+                        <div class="photo">
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/Left_side_of_Flying_Pigeon.jpg/1200px-Left_side_of_Flying_Pigeon.jpg" alt="">
+                        </div>
+
+                        <div class="photo">
+                            <img src="https://cdn.mos.cms.futurecdn.net/Ajc3ezCTN4FGz2vF4LpQn9-970-80.jpg.webp" alt="">
+                        </div>
+
+                        <div class="photo">
+                            <img src="https://img.lazcdn.com/g/p/216f90421777abcf04c759a789758ef9.jpg_720x720q80.jpg" alt="">
+                        </div>
+
+                        <div class="photo">
+                            <img src="https://www.cnet.com/a/img/resize/1baf0afc316334b1b3f8357238c339023b35bc5e/hub/2024/01/09/48730ab4-cd9f-401d-84a4-e1ffdac9e46c/anker-maggo-power-bank-10k-1.png?auto=webp&fit=crop&height=1200&width=1200" alt="">
+                        </div>
+                    </div>
+
+                    <div class="column">
+                        <div class="photo">
+                            <img src="https://assets.leevalley.com/Size4/10111/36K0402-laced-danish-cord-200m-d-0018.jpg" alt="">
+                        </div>
+
+                        <div class="photo">
+                            <img src="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSmSpEn3ITQmQmyX18cWsAR11Dna-ALj6fvoc0NSAp1Ih2k_Yp1Sts2ZH5Nq8-G9r29dorCrN6v1AYZGXnM2LUfOPbhH3-5L5DWnbKWo38M&usqp=CAE" alt="">
+                        </div>
+
+                        <div class="photo">
+                        <a download="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcS1cOKvrUDLmEkWg91-ra2oPfIIRj-xOSR7WTYVX9MNuaZYLsFu0gZNdyDdmv3bOXtXJFHwnaEopSpa-1YGdROWw96YTHiaeq-2x38tINS3&usqp=CAE" href="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcS1cOKvrUDLmEkWg91-ra2oPfIIRj-xOSR7WTYVX9MNuaZYLsFu0gZNdyDdmv3bOXtXJFHwnaEopSpa-1YGdROWw96YTHiaeq-2x38tINS3&usqp=CAE" data-sub-html=".caption" class="card ${item.theme_id}">
+                            <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcS1cOKvrUDLmEkWg91-ra2oPfIIRj-xOSR7WTYVX9MNuaZYLsFu0gZNdyDdmv3bOXtXJFHwnaEopSpa-1YGdROWw96YTHiaeq-2x38tINS3&usqp=CAE" />
+                            <div class="caption">
+                                <h6> ${Price}</h6>
+                                <p></p>
+                            </div>
+                        </a>
+                        
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </section>
+
+    <?php
+        include('../includes/footer.php'); 
+        ?>
+    <script> lightGallery(document.querySelector(".photo-gallery"), {
+            subHtmlSelectorRelative: true
+        }); 
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/lightgallery-js/1.4.0/js/lightgallery.min.js"></script>
+    </body>
+    </html>
