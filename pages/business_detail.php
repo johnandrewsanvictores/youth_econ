c534b51c2e3b81749a3b8e7de4cc4486ff24ce63
Rhesty <adormeorhesty@gmail.com>
2024-10-22 22:54:03 +0800

    new update 1

--- pages/business_detail.php
+++ pages/business_detail.php
@@ -1,14 +1,74 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+    <link rel="stylesheet" href="../global.css">
+    <link rel="stylesheet" href="../public/css/business_detail.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightgallery-js/1.4.0/css/lightgallery.min.css" />
+    <title>Youth Econ</title>
 </head>
+
 <body>
+    <?php
+        include('../includes/nav.php');
+    ?>
 
+    <section class="main-container">
+        <div class="business_Info">
+            <img src="../public/images/businesslogokape.png" alt="kape" id="businesslogo">
+            <h2 id="businessname">businessname "kape"</h2>
+            <p id="business_name_info">Lorem, ipsum dolor sit amet consectetur adipisicing dicta impedit voluptates laboriosam.</p>
+        </div>
 
-    sadasdasdasdasd
-    
+        <div class="Map">
+            <div class="map-contact-info">
+                    <div class="information">
+                        <div class="iconinfo">
+                            <i class="fas fa-map-marker-alt">   </i>
+                            <p> Brgy.Gulang Gulang Lucena, City </p>
+                        </div>
+                        <div class="iconinfo">
+                            <i class="fas fa-phone-alt"></i>
+                            <p> Brgy.Gulang Gulang Lucena, City </p>
+                        </div><div class="iconinfo">
+                            <i class="fab fa-facebook"></i>
+                            <p> Brgy.Gulang Gulang Lucena, City </p>
+                        </div>
+                    </div>
+                    <div class="map-section">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1936.197877911124!2d121.6159095227267!3d13.93502563844402!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd4b5a375ef9cd%3A0x48ff73822d713712!2sJoyCity%20General%20Merchandise!5e0!3m2!1sen!2sph!4v1727366933112!5m2!1sen!2sph" width="1000" height="500" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+            </div>
+        </div>
+        <div class="gallery-section">
+            <div class="img1">
+                <img src="../public/images/businesslogokape.png" alt="kape">
+            </div>
+            <div class="img2">
+                <img src="../public/images/businesslogokape.png" alt="kape">
+            </div>
+            <div class="img3">
+                <img src="../public/images/businesslogokape.png" alt="kape">
+            </div>
+            <div class="img4">
+                <img src="../public/images/businesslogokape.png" alt="kape">
+            </div>
+            <div class="img5">
+                <img src="../public/images/businesslogokape.png" alt="kape">
+            </div>
+            <div class="img6">
+                <img src="../public/images/businesslogokape.png" alt="kape">
+            </div>
+        </div>
+    </section>
+
+    <?php
+        include('../includes/footer.php');
+    ?>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/lightgallery-js/1.4.0/js/lightgallery.min.js"></script>
 </body>
+
 </html>
